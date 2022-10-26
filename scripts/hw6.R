7903306e653053afdb41df81aed1eb6e19a987e3
unknown <lplatt@usgs.gov>
2022-10-26 10:17:52 -0500

    refine optimal model

--- scripts/hw6.R
+++ scripts/hw6.R
@@ -10,10 +10,13 @@ library(nlme)
 # First subset the data so that we are considering only
 # one species (burbot) and one net type (sequential-5).
 fish_data <- read_csv('data/all-spp_leng-mesh_2009-2022.csv') %>% 
-  filter(net_type == "sequential-5") %>% 
-  filter(species == "burbot") %>%
+  filter(net_type == "sequential-5") %>%
   # This site only had one year of data, so remove it
-  filter(location != "Hatgal")
+  filter(location != "Hatgal") %>% 
+  group_by(species) %>%
+  mutate(count = n()) %>%
+  ungroup() %>% 
+  filter(species == "burbot")
 
 unique(fish_data$location) 
 # I don't think year gives us the nested structure we need for a hierarchical model
@@ -22,48 +25,49 @@ unique(fish_data$year)
 unique(fish_data$mesh_mm)
 
 # Crosswalking to RIKZ data terms:
-# Beach = location
-# Site = mesh_mm
+# Beach = year
+# Site = location
 # Y = length_mm (what are we saying length is related to?)
-# X = ?
-
-fish_data %>% 
-  group_by(location, mesh_mm) %>% 
-  tally()
+# X = mesh_mm
 
 ##### Figure out the appropriate model #####
 
 fish_data_ready <- fish_data %>% 
   # Sample has to have measured mesh size
   filter(!is.na(mesh_mm)) %>% 
-  mutate(floc = factor(location),
-         fmesh = factor(mesh_mm)) %>% 
-  select(year, floc, fmesh, fish_length = length_mm)
+  mutate(fyear = factor(year),
+         floc = factor(location),
+         mesh_size = mesh_mm)  %>%
+  select(fyear, floc, mesh_size, fish_length = length_mm)
 
 # No random effects
 
-m1 <- gls(fish_length ~ 1 + fmesh,
+m1 <- gls(fish_length ~ 1 + mesh_size * floc,
           method = "REML", data = fish_data_ready)
 
 # Add in random effects
-m2 <- lme(fish_length ~ 1 + fmesh,
-          random = ~ 1 | floc,
+m2 <- lme(fish_length ~ 1 + mesh_size * floc,
+          random = ~ 1 | fyear,
           method = "REML", data = fish_data_ready)
-m3 <- lme(fish_length ~ 1 + fmesh,
-          random = ~ 1 + fmesh | floc,
+m3 <- lme(fish_length ~ 1 + mesh_size * floc,
+          random = ~ 1 + mesh_size | fyear,
           method = "REML", data = fish_data_ready)
 
-summary(m1) # AIC = 1972.441
-summary(m2) # AIC = 1974.441
-summary(m3)
+summary(m1) # AIC = 1949.347
+summary(m2) # AIC = 1950.958
+summary(m3) # AIC = 1954.79
 
 # Choose the appropriate model based on lowest AIC. Then 
 # use `summary()` to drop individual terms from the model
 # based on their statistical significance using p-val
 summary(m1)
 
+# Only the mesh_size is statistically significant (p < 0.05)
+
 # Then write out the optimal model
-moptimal <- m1
+moptimal <- gls(fish_length ~ 1 + mesh_size,
+                method = "REML", data = fish_data_ready)
+summary(moptimal)
 
 ##### Run simulation #####
 
